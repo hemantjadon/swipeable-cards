4dd56d1c1d1852eba62559e94046b9b864cceadd
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-06-20 21:24:17 +0530

    Almost done

--- build/components/swipeable-cards/swipeable-cards.js
+++ build/components/swipeable-cards/swipeable-cards.js
@@ -11,24 +11,83 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var SwipeableCards = (function () {
     function SwipeableCards() {
+        this.target = null;
+        this.targetBCR = null;
+        this.startX = 0;
+        this.currentX = 0;
+        this.targetX = 0;
+        this.moveX = 0;
+        this.is_dragging = false;
+        this.onStart.bind(this);
+        this.onMove.bind(this);
+        this.onEnd.bind(this);
+        this.update.bind(this);
     }
     SwipeableCards.prototype.ngAfterContentInit = function () {
+        var _this = this;
         this.setEventlisteners();
+        window.requestAnimationFrame(function () { _this.update(); });
     };
     SwipeableCards.prototype.setEventlisteners = function () {
-        var cards_dom = document.querySelectorAll('md-swipeable-cards md-card');
-        var cards = Array.from(cards_dom);
-        cards.forEach(function () {
+        var _this = this;
+        var cards_elem_array = document.querySelectorAll('md-swipeable-cards md-card');
+        var cards = Array.from(cards_elem_array);
+        cards.forEach(function (card) {
+            card.addEventListener('touchstart', function (evt) { _this.onStart(evt, card); });
+            card.addEventListener('touchmove', function (evt) { _this.onMove(evt); });
+            card.addEventListener('touchend', function (evt) { _this.onEnd(evt); });
+            card.addEventListener('mousedown', function (evt) { _this.onStart(evt, card); });
+            card.addEventListener('mousemove', function (evt) { _this.onMove(evt); });
+            card.addEventListener('mouseup', function (evt) { _this.onEnd(evt); });
         });
+    };
+    SwipeableCards.prototype.onStart = function ($event, card) {
+        this.target = card;
+        this.targetBCR = this.target.getBoundingClientRect();
+        this.is_dragging = true;
+        this.startX = $event.pageX || $event.touches[0].pageX;
+        this.currentX = this.startX;
+        this.targetX = 0;
+        this.target.style.willChange = 'transform,opacity';
+        $event.preventDefault();
+    };
+    SwipeableCards.prototype.onMove = function ($event) {
+        if (!this.target) {
+            return;
+        }
+        this.currentX = $event.pageX || $event.touches[0].pageX;
+    };
+    SwipeableCards.prototype.onEnd = function ($event) {
+        if (!this.target) {
+            return;
+        }
+        if (Math.abs(this.moveX) > this.targetBCR.width * 0.35) {
+            this.targetX = (this.moveX > 0) ? this.targetBCR.width : -this.targetBCR.width;
+        }
+        this.is_dragging = false;
+    };
+    SwipeableCards.prototype.update = function () {
+        var _this = this;
+        window.requestAnimationFrame(function () { _this.update(); });
+        if (!this.target) {
+            return;
+        }
+        if (this.is_dragging) {
+            this.moveX = this.currentX - this.startX;
+        }
+        else {
+            this.moveX += (this.targetX - this.moveX) / 4;
+        }
+        var normalizedDragDistance = Math.abs(this.moveX / this.targetBCR.width);
+        var opacity = 1 - Math.pow(normalizedDragDistance, 3);
+        this.target.style.transform = "translateX(" + this.moveX + "px)";
+        this.target.style.opacity = opacity;
     };
     SwipeableCards = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'md-swipeable-cards',
             template: '<ng-content></ng-content>',
-            styles: [
-                "\n\t\t\t:host md-card{\n\t\t\t\twill-change: transform,opacity;\n\t\t\t}\n\t\t"
-            ]
         }), 
         __metadata('design:paramtypes', [])
     ], SwipeableCards);
--- components/swipeable-cards/swipeable-cards.ts
+++ components/swipeable-cards/swipeable-cards.ts
@@ -4,26 +4,95 @@ import { Component,AfterContentInit } from '@angular/core';
 	moduleId: module.id,
 	selector: 'md-swipeable-cards',
 	template: '<ng-content></ng-content>',
-	styles: [
-		`
-			:host md-card{
-				will-change: transform,opacity;
-			}
-		`
-	]
 })
 
 export class SwipeableCards implements AfterContentInit{
+	constructor() {
+		this.onStart.bind(this);
+		this.onMove.bind(this);
+		this.onEnd.bind(this);
+		this.update.bind(this);
+	}
 	ngAfterContentInit(){
 		this.setEventlisteners();
+		window.requestAnimationFrame(()=>{ this.update(); });
 	}
 
+	private target = null;
+
+	private targetBCR = null;
+
+	private startX : number = 0;
+
+	private currentX : number = 0;
+
+	private targetX : number = 0;
+
+	private moveX : number = 0;
+
+	private is_dragging : boolean = false;
+
 	private setEventlisteners() {
-		let cards_dom = document.querySelectorAll('md-swipeable-cards md-card');
-		let cards = Array.from(cards_dom);
+		let cards_elem_array = document.querySelectorAll('md-swipeable-cards md-card');
+		let cards = Array.from(cards_elem_array);
 
-		cards.forEach(() => {
+		cards.forEach((card) => {
+			card.addEventListener('touchstart', (evt) => { this.onStart(evt,card) });
+			card.addEventListener('touchmove', (evt) => { this.onMove(evt) });
+			card.addEventListener('touchend', (evt) => { this.onEnd(evt) });
 
+			card.addEventListener('mousedown', (evt) => { this.onStart(evt,card) });
+			card.addEventListener('mousemove', (evt) => { this.onMove(evt) });
+			card.addEventListener('mouseup', (evt) => { this.onEnd(evt) });
 		});
+	}
+
+	private onStart ($event,card) {
+		this.target = card;
+		this.targetBCR = this.target.getBoundingClientRect();
+
+		this.is_dragging = true;
+		this.startX = $event.pageX || $event.touches[0].pageX;
+		this.currentX = this.startX;
+		this.targetX = 0;
+
+		this.target.style.willChange = 'transform,opacity'
+
+		$event.preventDefault();
+	}
+
+	private onMove ($event) {
+		if (!this.target) { return ; }
+
+		this.currentX = $event.pageX || $event.touches[0].pageX;
+	}
+
+	private onEnd ($event) {
+		if (!this.target) { return ; }
+
+		if (Math.abs(this.moveX) > this.targetBCR.width * 0.35) {
+			this.targetX = (this.moveX > 0) ? this.targetBCR.width : -this.targetBCR.width;
+		}
+
+		this.is_dragging = false;
+	}
+
+	public update() {
+		window.requestAnimationFrame(()=>{ this.update(); });
+
+		if (!this.target) { return ; }
+
+		if (this.is_dragging){
+			this.moveX = this.currentX - this.startX;
+		}
+		else {
+			this.moveX += (this.targetX - this.moveX)/4;
+		}
+
+		const normalizedDragDistance = Math.abs(this.moveX/this.targetBCR.width);
+		const opacity = 1 - Math.pow(normalizedDragDistance,3);
+
+		this.target.style.transform = `translateX(${this.moveX}px)`;
+		this.target.style.opacity = opacity;
 	}
 }
